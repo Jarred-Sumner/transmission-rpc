c01d237e3948d6b8b92e8f98dddf2da998c6b1fb
Alex Chrome <dify.chrome@gmail.com>
2014-04-30 22:20:22 +0700

    Added ability to pass addition options to 'torrent-add' method

--- lib/transmission-rpc/torrent.rb
+++ lib/transmission-rpc/torrent.rb
@@ -4,33 +4,33 @@ module Transmission
     class Torrent
       ATTRIBUTES = {
         :id                 => 'id',
-        :date_added         => 'addedDate', 
-        :comment            => 'comment', 
-        :description        => 'description', 
+        :date_added         => 'addedDate',
+        :comment            => 'comment',
+        :description        => 'description',
         :download_directory => 'downloadDir',
-        :date_done          => 'doneDate', 
-        :eta                => 'eta', 
-        :files              => 'files', 
-        :hash               => 'hashString', 
+        :date_done          => 'doneDate',
+        :eta                => 'eta',
+        :files              => 'files',
+        :hash               => 'hashString',
         :leechers           => 'leechers',
-        :bytes_left         => 'leftUntilDone', 
+        :bytes_left         => 'leftUntilDone',
         :name               => 'name',
-        :percent_done       => 'percentDone', 
-        :download_speed     => 'rateDownload', 
+        :percent_done       => 'percentDone',
+        :download_speed     => 'rateDownload',
         :seeders            => 'seeders',
         :seed_ratio_limit   => 'seedRatioLimit',
-        :size               => 'sizeWhenDone', 
-        :status             => 'status', 
-        :torrent_file       => 'torrentFile', 
-        :total_size         => 'totalSize', 
+        :size               => 'sizeWhenDone',
+        :status             => 'status',
+        :torrent_file       => 'torrentFile',
+        :total_size         => 'totalSize',
         :upload_limit       => 'uploadLimit',
         :upload_limited     => 'uploadLimited',
         :upload_ratio       => 'uploadRatio'
-      } 
+      }
 
       ATTRIBUTES.each {|k,v| attr_accessor k}
       include Transmission::RPC
-      
+
       def initialize(options = {})
         ATTRIBUTES.each do |k,v|
           self.send("#{k}=", options[v])
@@ -59,20 +59,20 @@ module Transmission
         self.status == 4
       end
 
-      # Checks if torrent is paused 
+      # Checks if torrent is paused
       def paused?
         self.status == 0
       end
 
       # Adds a torrent by URL or file path
-      def self.+(url)
-        self.add(:url => url)
+      def self.+(filename)
+        self.add(:filename => filename)
       end
 
       # Gets all the torrents
       def self.all
         @unprocessed_torrents = Client.request("torrent-get", { :fields => self.fields })['arguments']['torrents']
-        @unprocessed_torrents.collect { |torrent| self.new(torrent) }        
+        @unprocessed_torrents.collect { |torrent| self.new(torrent) }
       end
 
       # Finds a torrent by ID
@@ -89,11 +89,10 @@ module Transmission
       # Adds a torrent by file path or URL (.torrent file's only right now)
       def self.add(options = {})
         @response = nil
-        if options[:metainfo]
-          @response = Client.request("torrent-add", :metainfo => options[:metainfo])
-        else
-          @response = Client.request("torrent-add", :filename => options[:url])
-        end
+        options[:filename] ||= options.delete(:url) if options[:url] # for backward compatibility
+        options = Hash[options.map { |key, value| [key.to_s.gsub('_', '-').to_sym, value] }]
+
+        @response = Client.request("torrent-add", options)
 
         if @response['result'] == 'success'
           self.find(@response['arguments']['torrent-added']['id'])
